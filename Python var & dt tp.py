dc79c7f076573933b56766036cef06e949184829
pc  le novo <messghir4@gmail.com>
2023-12-04 14:56:43 +0100

    Python var & dt tp.py

---
+++ Python var & dt tp.py
@@ -0,0 +1,10 @@
+
+character_name = "Tom"
+character_age = "50"
+old = "true"
+print("There once was a man named " + character_name + ", ")
+print("He was " + character_age + " yo. ")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
