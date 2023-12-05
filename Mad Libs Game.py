bd86616da6586cbb0780cc170bc172cb51885cc1
pc  le novo <messghir4@gmail.com>
2023-12-05 11:51:34 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ Mad Libs Game.py
@@ -0,0 +1,11 @@
+
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+person = input("Enter a person's name: ")
+
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print(person + " I miss you <3")
+
